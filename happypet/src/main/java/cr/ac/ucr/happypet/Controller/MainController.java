6da866f008ec05feb70bddf1e60a92ba624c85a7
Luis <Luis.leiton.cr@gmail.com>
2021-07-05 00:56:18 -0600

    Cambios de facturacion
    Mas cambios en la facturación para probar tablas y otros detalles.

--- happypet/src/main/java/cr/ac/ucr/happypet/Controller/MainController.java
+++ happypet/src/main/java/cr/ac/ucr/happypet/Controller/MainController.java
@@ -4,7 +4,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import cr.ac.ucr.happypet.Model.billing.Line;
 import cr.ac.ucr.happypet.Model.billing.ShoppingCart;
+import cr.ac.ucr.happypet.Model.products.Product;
 import cr.ac.ucr.happypet.Model.users.User;
 
 @SessionAttributes("shoppingCart, activeUser")
@@ -15,12 +17,21 @@ public class MainController {
 
         if (shoppingCart == null) {
             shoppingCart = new ShoppingCart();
+
+            Line ln = new Line();
+            Product pr = new Product("Mimados", 2500, "Alimento para cachorro", "Perro");
+            pr.setCod_product(1);
+            ln.setProduct(pr);
+            ln.setQuantity(5);
+
+            shoppingCart.addLine(ln);
+
         }
         return shoppingCart;
     }
 
     @ModelAttribute("activeUser")
-    public User shoppingCart(@SessionAttribute(required = false) User activeUser) {
+    public User activeUser(@SessionAttribute(required = false) User activeUser) {
 
         if (activeUser == null) {
             activeUser = new User();
